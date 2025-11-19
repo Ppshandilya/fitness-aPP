1423b65b75b9c18233d928b20d05116179fb27b9
Ppshandilya Upadhyay <ppsupadhyay@gmail.com>
2025-11-20 00:36:58 +0530

    deploying to test3

--- app/models/workout.py
+++ app/models/workout.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, APIRouter
 from fastapi.responses import HTMLResponse
 
 from typing import Dict
-from database import get_db
+from app.database import get_db
 from pydantic import BaseModel
 
 from sqlalchemy import Column, Integer, String,Boolean,ForeignKey,Text,create_engine
@@ -22,6 +22,17 @@ class Workout(Base):
     intensity = Column(Integer, nullable=False)
    
 
+# class Circles(Base):
+#     __tablename__ = 'circle'
+#     id = Column(Integer, primary_key=True, index=True)
+#     circle_name = Column(String(50),  index=True)
+#     recipients = Column(String(200)) 
+
+# class Accounts(Base):
+#     __tablename__ = 'accounts'
+#     id = Column(Integer, primary_key=True, index=True)
+#     login_name= Column(String(50),  index=True)
+#     login_password= Column(String(200)) 
 
     
 
