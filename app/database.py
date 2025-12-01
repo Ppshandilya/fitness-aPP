ce446b333282e240226684e00de33e1a17214605
Ppshandilya Upadhyay <ppsupadhyay@gmail.com>
2025-12-01 12:52:49 +0530

    deploy 2

--- app/database.py
+++ app/database.py
@@ -3,10 +3,11 @@ import MySQLdb
 from contextlib import contextmanager
 import base64
 
-SessionLocal = sessionmaker(bind=engine)
+
 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+
 
 
 # RDS connection details
